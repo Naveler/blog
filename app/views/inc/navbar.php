6a87d018b6eefdf395dcb1300f8fb96940b34278
Naveler <georg.vilja@khk.ee>
2021-05-27 17:09:54 +0300

    Lisatud rakenduse navigatsiooni kasutajate haldusega seotud elemendid

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -10,6 +10,14 @@
                     <a href="<?php echo URLROOT;?>" class="nav-link">Home</a>
                 </li>
             </ul>
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a href="<?php echo URLROOT;?>/users/regiter" class="nav-link">Register</a>
+                </li>
+                <li class="nav-item">
+                    <a href="<?php echo URLROOT;?>/users/login" class="nav-link">Login</a>
+                </li>
+            </ul>
         </div>
     </div>
 </nav>
